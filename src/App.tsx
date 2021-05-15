4454574274acdea22fb832967cce2de12c1dbc7c
DominiquevdLinden <vdlindendominique33@gmail.com>
2021-05-15 21:28:36 +0200

    redirects

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="main">
+        <h1 id="title">Survey Form</h1>
+        <p id="description">hi</p>
+        <form id="survey-form">
+          <label id="name-label" htmlFor="name">
+            name
+          </label>
+          <input id="name" type="text" placeholder="name" required></input>
+          <label id="email-label" htmlFor="email">
+            email
+          </label>
+          <input id="email" placeholder="email" type="email" required></input>
+          <label id="number-label" htmlFor="number">
+            number
+          </label>
+          <input
+            type="number"
+            id="number"
+            placeholder="number"
+            min="6"
+            max="20"
+          ></input>
+          <select id="dropdown">
+            <option>hi</option>
+            <option>hello</option>
+          </select>
+          <input type="radio" name="radio" value="first"></input>
+          <input type="radio" name="radio" value="second"></input>
+          <input type="checkbox" value="first"></input>
+          <input type="checkbox" value="second"></input>
+          <textarea></textarea>
+          <button id="submit">submit</button>
+        </form>
+      </div>
     </div>
   );
 }
